f2f1592089ffc72cab01b3309ea6c2932e0e0f5c
Kelvin Kipchumba <chogekelvin90@gmail.com>
2026-09-26 11:16:52 +0300

    Configure environment-based database settings

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine as sqlalchemy_create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+
+load_dotenv()
 
 class Base(DeclarativeBase):
     pass
